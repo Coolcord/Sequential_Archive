a7fb66a63908bff4101beae626d5560c82f30fd8
Coolcord <coolcord24@gmail.com>
2015-09-22 19:15:46 -0700

    Defined some more functions

--- Reader.cpp
+++ Reader.cpp
@@ -2,7 +2,7 @@
 #include "Scrambler.h"
 #include "Common_Strings.h"
 #include "Common_Data.h"
-#include <QLinkedList>
+#include <QDir>
 #include <assert.h>
 
 Reader::Reader(const QString &archiveLocation) {
@@ -67,7 +67,27 @@ bool Reader::Extract_Directory(const QString &directoryPathInArchive, const QStr
     assert(this->file);
     assert(this->file->isOpen() && this->file->isReadable());
 
-    //TODO: Write this...
+    //Create a folder to extract the files into
+    QDir dir(destination);
+    if (!dir.exists() && !dir.mkdir(destination + "/" + (this->Get_File_Name_From_Path(directoryPathInArchive)))) return false;
+
+    //Extract all of the files first
+    QStringList files = this->Get_Files(directoryPathInArchive);
+    foreach (QString file, files) {
+        if (!this->Extract_File(directoryPathInArchive + "/" + file, destination + "/" + file)) return false;
+    }
+
+    //Then extract all of the directories
+    QStringList directories = this->Get_Directories(directoryPathInArchive);
+    foreach (QString directory, directories) {
+        if (!this->Extract_Directory(directoryPathInArchive + "/" + directory, destination + "/" + directory)) return false;
+    }
+
+    return true;
+}
+
+bool Reader::Extract_File_With_Buffer(const QString &filePathInArchive, const QString &destination) {
+
 }
 
 bool Reader::Is_Archive_Valid() {
@@ -109,14 +129,30 @@ bool Reader::Read_Scramble_Key(unsigned char &scrambleKey) {
     return true;
 }
 
+QString Reader::Get_File_Name_From_Path(const QString &path) {
+    return path.split('/').last();
+}
+
+QByteArray Reader::Read_Bytes(qint64 offset, qint64 size) {
+    if (!this->file->seek(offset)) return QByteArray();
+
+    //Read the bytes from the archive
+    QByteArray buffer = this->file->read(size);
+    if (buffer.isEmpty()) return QByteArray();
+
+    //Unscramble the bytes if necessary
+    if (this->scrambler) this->scrambler->Unscramble(buffer, offset);
+    return buffer;
+}
+
 //Avoid using this function unless absolutely necessary!
 //I may remove this later if it is not needed
 QByteArray Reader::Read_Until_End_Byte(qint64 offset) {
-    if (!this->file->seek(offset)) return false;
+    if (!this->file->seek(offset)) return QByteArray();
 
-    //We are using a linked list here as its append is constant time
+    //We are using a QString here as its append is constant time
     //QByteArray's append is linear time, which we want to avoid
-    QLinkedList byteList<char>();
+    QString byteList = "";
 
     //Read into the byteList until either the end byte is found or we are at the end of the file
     while (true) {
@@ -129,14 +165,6 @@ QByteArray Reader::Read_Until_End_Byte(qint64 offset) {
         ++offset;
     }
 
-    //Copy the unscrambled data into a proper byte array
-    QByteArray buffer(byteList.size(), ' ');
-    qint64 i = 0;
-    foreach (char c, byteList) {
-        buffer.data()[i] = c;
-        ++i;
-    }
-    assert(i == buffer.size()-1);
-    return buffer;
+    return QByteArray(byteList.toUtf8().data());
 }
 
--- Reader.h
+++ Reader.h
@@ -24,8 +24,11 @@ public:
     bool Extract_Directory(const QString &directoryPathInArchive, const QString &destination);
 
 private:
+    bool Extract_File_With_Buffer(const QString &filePathInArchive, const QString &destination);
     bool Is_Archive_Valid();
     bool Read_Scramble_Key(unsigned char &scrambleKey);
+    QString Get_File_Name_From_Path(const QString &path);
+    QByteArray Read_Bytes(qint64 offset, qint64 size);
     QByteArray Read_Until_End_Byte(qint64 offset); //try to avoid using this function!
 
     QFile *file;
