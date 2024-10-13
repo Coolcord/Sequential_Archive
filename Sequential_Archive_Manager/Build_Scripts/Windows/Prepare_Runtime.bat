001d3ca80f7e30905837459b0990f00716c0de41
Coolcord <coolcord24@tutanota.com>
2024-10-12 21:38:50 -0700

    Updated Windows Prepare_Runtime.bat to Qt version 6.7.3

--- Sequential_Archive_Manager/Build_Scripts/Windows/Prepare_Runtime.bat
+++ Sequential_Archive_Manager/Build_Scripts/Windows/Prepare_Runtime.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 COLOR 0A
 
-SET QT=Qt_6_5_2_MinGW_64_bit
+SET QT=Qt_6_7_3_MinGW_64_bit
 SET MODE=%1
 IF %1.==. SET MODE=Debug
 SET SOURCE_LOCATION=D:\Documents\Source_Code
 
 REM Prepare Folders
-MKDIR "%SOURCE_LOCATION%\Sequential_Archive\build-Sequential_Archive_Manager-Desktop_%QT%-%MODE%\Plugins" >NUL 2>&1
+MKDIR "%SOURCE_LOCATION%\Sequential_Archive\Sequential_Archive_Manager\build\Desktop_%QT%-%MODE%\Plugins" >NUL 2>&1
 
 REM Delete Old Plugins
-ERASE "%SOURCE_LOCATION%\Sequential_Archive\build-Sequential_Archive_Manager-Desktop_%QT%-%MODE%\Plugins\Sequential_Archive.dll" >NUL 2>&1
+ERASE "%SOURCE_LOCATION%\Sequential_Archive\Sequential_Archive_Manager\build\Desktop_%QT%-%MODE%\Plugins\Sequential_Archive.dll" >NUL 2>&1
 
 REM Install New Plugins
-COPY "%SOURCE_LOCATION%\Sequential_Archive\build-Sequential_Archive-Desktop_%QT%-%MODE%\libSequential_Archive.dll" "%SOURCE_LOCATION%\Sequential_Archive\build-Sequential_Archive_Manager-Desktop_%QT%-%MODE%\Plugins\Sequential_Archive.dll"
+COPY "%SOURCE_LOCATION%\Sequential_Archive\Sequential_Archive\build\Desktop_%QT%-%MODE%\libSequential_Archive.dll" "%SOURCE_LOCATION%\Sequential_Archive\Sequential_Archive_Manager\build\Desktop_%QT%-%MODE%\Plugins\Sequential_Archive.dll"
